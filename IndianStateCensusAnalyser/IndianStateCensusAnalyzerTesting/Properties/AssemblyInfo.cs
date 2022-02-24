2648210c40fe02ce262c6db8a7333e554cceac6a
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-02-25 00:45:19 +0530

    [Rajeshwari] adding Load Indian state census info

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyzerTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IndianStateCensusAnalyzerTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("IndianStateCensusAnalyzerTesting")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ed612e5-4e2c-47b0-b703-1fa3629f40a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
